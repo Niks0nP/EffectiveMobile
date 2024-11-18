12b2bde9b0627c77dfee2d3f37ec1d8c276adab9
Niks0nP <nikita.niko40@gmail.com>
2024-11-18 12:11:25 +0300

    update viewModel

--- presentation/src/main/java/com/niks0n/effectivemobile/ui/feature/search/SearchViewModel.kt
+++ presentation/src/main/java/com/niks0n/effectivemobile/ui/feature/search/SearchViewModel.kt
@@ -8,7 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
@@ -16,9 +16,7 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     repository: VacanciesRepository
 ) : ViewModel() {
-    val state: StateFlow<MainScreenState> = combine(
-        repository.getVacanciesInfo()
-    ) { jobInfo ->
+    val state: StateFlow<MainScreenState> = repository.getVacanciesInfo().map { jobInfo ->
         MainScreenState.Success(
             jobInfo = jobInfo
         )
@@ -34,18 +32,6 @@ class HomeViewModel @Inject constructor(
 sealed interface MainScreenState {
     data object Loading : MainScreenState
     data class Success(
-        val jobInfo: Array<JobDataModel>
-    ) : MainScreenState {
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as Success
-
-            return jobInfo.contentEquals(other.jobInfo)
-        }
-        override fun hashCode(): Int {
-            return jobInfo.contentHashCode()
-        }
-    }
+        val jobInfo: JobDataModel
+    ) : MainScreenState
 }
